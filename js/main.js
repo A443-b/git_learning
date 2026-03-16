27c30cd847d7ebff0fde793ec6fb8a19daed5f19
xinming <xinming@qq.com>
2026-03-16 11:00:07 +0800

    Add JavaScript files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// 主 JavaScript 文件
+console.log('Hello from main.js');
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('页面加载完成');
+});
---
+++ js/utils.js
@@ -0,0 +1,8 @@
+// 工具函数
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+function add(a, b) {
+    return a + b;
+}
